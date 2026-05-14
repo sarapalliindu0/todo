05c3c28748dfe40919b7e74d3f64bec9d408904f
sarapalliindu0 <sarapalliindu0@gmail.com>
2026-05-14 12:39:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function addTask(){
+
+    let input = document.getElementById("taskInput");
+    let task = input.value;
+
+    if(task === ""){
+        alert("Please enter a task");
+        return;
+    }
+
+    let li = document.createElement("li");
+    li.innerHTML = `
+        <span onclick="toggleTask(this)">${task}</span>
+        <button onclick="deleteTask(this)">X</button>
+    `;
+
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
+}
+
+function deleteTask(button){
+    button.parentElement.remove();
+}
+
+function toggleTask(task){
+    task.classList.toggle("completed");
+}
